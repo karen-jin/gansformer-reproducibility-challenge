23dba59241e6c11596a9df94fdd8f32f5ca4b09c
Karen Jin <karen@jasmine.local>
2022-04-11 10:31:11 -0400

    add files

---
+++ README.md
@@ -0,0 +1,33 @@
+# adapted from gansformer-reproducibility-challenge
+
+> Project for Advance Topic in Machine Learning course @ USI 21/22.  
+> See https://github.com/GiorgiaAuroraAdorni/gansformer-reproducibility-challenge, https://drive.google.com/drive/folders/1sqHD-X4mLOOkoT-xJvWGdPlwxb5et0kA?usp=sharing for datasets and https://drive.google.com/drive/folders/1ZFfO4HVINH-aDQbgLscJxNTqGLEIOMZv?usp=sharing for models.
+
+### Contributors
+
+**Giorgia Adorni** — giorgia.adorni@usi.ch [GiorgiaAuroraAdorni](https://github.com/GiorgiaAuroraAdorni)
+
+**Felix Boelter** — felix.boelter@usi.ch [felixboelter](https://github.com/felixboelter)
+
+**Stefano Carlo Lambertenghi** — stefano.carlo.lambertenghi@usi.ch [steflamb](https://github.com/steflamb)
+
+### Prerequisites
+
+- Python 3
+- Tensorflow 1.X
+
+### Installation
+
+Clone our repository and install the requirements
+
+```sh
+$ git clone https://github.com/GiorgiaAuroraAdorni/gansformer-reproducibility-challenge
+$ cd gansformer-reproducibility-challenge/src
+$ pip install -r requirements.txt
+```
+
+#### Usage
+
+For the usage, go to the `colab notebooks` directory: 
+- Run `Reproducibility_model_trainer.ipynb` for training the models: Stylegan2, GANformers with Simplex and Duplex Attention and GANformers with Simplex and Duplex Attention (with vanilla StyleGAN2 discriminator).  
+- Run `Reproducibility_result_visualizer.ipynb` for the visualisation phase: here you can select the model that you want to use and generate random images, perform a symple interpolation of the latent space or even perform style mixing starting from a chosen target image.
---
+++ src/trained_network/plots.py
@@ -0,0 +1,113 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+matplotlib.pyplot.title(r'ABC123 vs $\mathrm{ABC123}^{123}$')
+
+def annotate_lasts(ys, ax=None):
+    for idx, ymin in enumerate(ys):
+        text = "{:.0f}".format(ymin)
+
+        if not ax:
+            ax = plt.gca()
+
+        multip = 7 * idx
+
+        ax.annotate(text, xy=(300, ymin), xycoords='data',
+                    xytext=(20, -10 + multip), textcoords='offset points',
+                    arrowprops=dict(arrowstyle="->")
+                    )
+
+def plot_learning_curve(data, labels, title):
+    """
+    Generate a simple plot of the learning performance.
+
+    @param data:
+    @param labels:
+    @param title:
+    @return: plot
+    """
+    plt.figure()
+    plt.title(title, fontweight="bold")
+
+    plt.xlabel("Step")
+    plt.ylabel("FID score")
+
+    plt.semilogy()
+
+    ticks = np.arange(0, 500, 50)
+    plt.yticks(ticks, ticks)
+
+    plt.xlim(left=-10, right=345)
+
+    plt.grid()
+
+    y_lasts = []
+    for idx, el in enumerate(data):
+        plt.plot(el['snapshot'], el['fid10k'], 'o-',  label=labels[idx], ms=3)
+        y_lasts.append(el['fid10k'][19])
+
+    annotate_lasts(np.sort(y_lasts))
+
+    # plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: '{:g}'.format(y)))
+    # plt.gca().yaxis. set_major_formatter(
+    #     ticker.FuncFormatter(lambda y, pos: ('{{:.{:1d}f}}'.format(int(np.maximum(-np.log10(y), 0)))).format(y)))
+
+    plt.legend()
+    # plt.legend(loc='center', bbox_to_anchor=(1.35, 0.5))
+    return plt
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Google Cartoon Set
+
+# opening the CSV file
+data_stylegan = pd.read_csv('scores/FIDScore_Stylegan2_300kimg.csv', delimiter=" ")
+data_ganf_simp_n_att = pd.read_csv('scores/FIDScore_GanformerSimplexNoAtt_300kimg.csv', delimiter=" ")
+data_ganf_simp_att = pd.read_csv('scores/FIDScore_GanformerSimplexAtt_300kimg.csv', delimiter=" ")
+data_ganf_dupl_n_att = pd.read_csv('scores/FIDScore_GanformerDuplexNoAtt_300kimg.csv', delimiter=" ")
+data_ganf_dupl_att = pd.read_csv('scores/FIDScore_GanformerDuplexAtt_300kimg.csv', delimiter=" ")
+
+labels = ["StyleGAN2",
+          "GANformer, Simplex attention \n(Stylegan2 discriminator)",
+          "GANformer, Simplex attention",
+          "GANformer, Duplex attention \n(Stylegan2 discriminator)",
+          "GANformer, Duplex attention"]
+
+title = "Learning Performance"
+# Cross validation with 100 iterations to get smoother mean test and train
+# score curves, each time with 20% data randomly selected as a validation set.
+data = [data_stylegan, data_ganf_simp_n_att, data_ganf_simp_att, data_ganf_dupl_n_att, data_ganf_dupl_att]
+plot_learning_curve(data, labels, title)
+
+plt.savefig('out_imgs/FIDscore-cartoonset.pdf', dpi=600, bbox_inches='tight')
+plt.close()
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# FFHQ
+
+data_stylegan = pd.read_csv('scores/ffhq-fid-stylegan2.csv', delimiter=" ")
+data_ganf_simp_n_att = pd.read_csv('scores/ffhq-fid-GanformerSimplexNoAtt.csv', delimiter=" ")
+data_ganf_simp_att = pd.read_csv('scores/ffhq-fid-GanformerSimplexAtt.csv', delimiter=" ")
+data_ganf_dupl_n_att = pd.read_csv('scores/ffhq-fid-GanformerDuplexNoAtt.csv', delimiter=" ")
+data_ganf_dupl_att = pd.read_csv('scores/ffhq-fid-GanformerDuplexAtt.csv', delimiter=" ")
+
+labels = ["StyleGAN2",
+          "GANformer, Simplex attention \n(Stylegan2 discriminator)",
+          "GANformer, Simplex attention",
+          "GANformer, Duplex attention \n(Stylegan2 discriminator)",
+          "GANformer, Duplex attention"]
+
+
+title = "Learning Performance"
+# Cross validation with 100 iterations to get smoother mean test and train
+# score curves, each time with 20% data randomly selected as a validation set.
+data = [data_stylegan, data_ganf_simp_n_att, data_ganf_simp_att, data_ganf_dupl_n_att, data_ganf_dupl_att]
+plot_learning_curve(data, labels, title)
+
+plt.savefig('out_imgs/FIDscore-ffhq.pdf', dpi=600, bbox_inches='tight')
+plt.close()
